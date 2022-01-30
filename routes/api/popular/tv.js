d60cc0cb1d4d41b87fa6d167288ca687922433f3
DmanCoder <ktdenis.1@live.com>
2022-01-30 19:47:39 +1000

    Added routs for todo

--- routes/api/popular/tv.js
+++ routes/api/popular/tv.js
@@ -1,6 +1,12 @@
 const { dbAPI, axios } = require('../../../api/init');
 const express = require('express');
 const url = require('url');
+TODO: https://www.themoviedb.org/talk/6002a239223e20003fb6e10b
+/*
+ * A query looking for popular streaming titles: https://api.themoviedb.org/3/discover/movie?api_key=###&watch_region=US&with_watch_monetization_types=flatrate
+ * A query looking for popular titles available for rent:https://api.themoviedb.org/3/discover/movie?api_key=###&watch_region=US&with_watch_monetization_types=rent
+ * A query looking for movies in the theatres: https://api.themoviedb.org/3/discover/movie?api_key=###&region=US&with_release_type=3|2
+ */
 
 const router = express.Router();
 
