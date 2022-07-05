4eb49b58ab36c84c1ffb302c6326a250d8708fe2
denis.otim <denis.otim@betmakers.com>
2022-07-05 17:23:11 +1000

    Test commit

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send({
     message: 'hello world this is me life should be fun for everyone (The Saddle Club)',
+    test: 'testing...'
   });
 });
 app.use('/api/popular/tv', require('./routes/api/tv'));
