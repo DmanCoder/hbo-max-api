5db89a65e6f4bae92789f7e36447f1971e37f3f9
denis.otim <denis.otim@betmakers.com>
2022-07-12 17:32:00 +1000

    Setp validations

--- routes/api/details.js
+++ routes/api/details.js
@@ -11,7 +11,7 @@ const url = require('url');
 const router = express.Router();
 
 // Validators
-const validatePopularTv = require('../../validations/popular/tv');
+const validateMediaDetails = require('../../validations/validateMediaDetails');
 
 /**
  * @dec       This API makes a request to TMDb API and returns the request
@@ -27,11 +27,11 @@ router.get('/', (req, res) => {
   const { language, page, media_id } = queryObject;
 
   // Reject if expected params are not present
-  // const { errors, isValid } = validatePopularTv(queryObject);
-  // if (!isValid) {
-  //   res.status(400);
-  //   return res.send({ errors });
-  // }
+  const { errors, isValid } = validateMediaDetails(queryObject);
+  if (!isValid) {
+    res.status(400);
+    return res.send({ errors });
+  }
 
   // Get popular tv shows
   const epTv = `/tv/${media_id}?api_key=${process.env.THE_MOVIE_DATABASE_API}&languages=${language}&pages=${page}`;
